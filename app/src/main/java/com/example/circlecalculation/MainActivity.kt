b9edbf1c3603bf1730571e188587ad2e04b31900
Danish Khan <iamdanishk555@gmail.com>
2022-06-29 16:03:14 +0545

    Test Cases written for MyCalc class

--- app/src/main/java/com/example/circlecalculation/MainActivity.kt
+++ app/src/main/java/com/example/circlecalculation/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.circlecalculation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.example.circlecalculation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: CalcViewModel
+    lateinit var factory: CalcViewModelFactory
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        factory = CalcViewModelFactory(MyCalc())
+        viewModel = ViewModelProvider(this, factory).get(CalcViewModel::class.java)
+        binding.myViewModel = viewModel
+        binding.lifecycleOwner = this
     }
 }
