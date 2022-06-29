a639d8e2ddfb977232373697510d3e975941d087
Danish Khan <iamdanishk555@gmail.com>
2022-06-29 15:12:25 +0545

    ViewModel and ViewModelFactory classes created

---
+++ app/src/main/java/com/example/circlecalculation/CalcViewModelFactory.kt
@@ -0,0 +1,14 @@
+package com.example.circlecalculation
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class CalcViewModelFactory(
+    private val calculations: Calculations
+) : ViewModelProvider.Factory  {
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return CalcViewModel(calculations) as T
+    }
+
+}
